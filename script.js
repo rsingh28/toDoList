134ed250540394b45acf4076bfda921edf501409
rsingh28 <rashmeet.singh28@gmail.com>
2017-10-30 00:05:57 -0400

    Adding a check mark in front of the added element as a marker

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function addAction () {
-  var action = document.getElementById("action").value;  // Get the text from the text bar
-  var newli = document.createElement('li');              // Creating a new list item
-  var t = document.createTextNode(action);               // Create a text node with the text from text bar
-  //document.body.appendChild(t);
-  newli.appendChild(t);                                  // Appending the text node to the new list item 
-  document.getElementById("list").appendChild(newli);    // Appending the list item to the main list 
-  document.getElementById("action").value = "";          // Setting the text bar to blank
+// Adding a check mark in front of the default element in the list (Repeated in the addAction() function - to add a check mark everytime an element is added)
+
+var elementList = document.getElementsByTagName("LI");       // Make an array of elements currently in the to-do list 
+for(var i=0 ; i<elementList.length ; i++){
+  var newSpan = document.createElement("SPAN");              // New span element
+  var checkMark = document.createTextNode("     \u2713");    // Creating a check mark using uni-code
+  newSpan.className = "close";                               // Will be used furthur to basically remove the item on clicking the check mark
+  newSpan.appendChild(checkMark);                            // Append the check mark to the new spam element
+  elementList[i].appendChild(newSpan);                       // Append the spam element (the check mark) to the list element 
 }
 
+// Adding a new element to the list on clicking the button 
+
+function addAction () {
+  var action = document.getElementById("action").value;       // Get the text from the text bar
+  var newli = document.createElement('li');                   // Creating a new list item
+  var t = document.createTextNode(action);                    // Create a text node with the text from text bar
+  newli.appendChild(t);                                       // Setting the new list item with the value of text node 
+  document.getElementById("list").appendChild(newli);         // Appending the list item to the main list 
+  document.getElementById("action").value = "";               // Setting the text bar to blank
+
+  // Adding a check mark in front of the added element 
+  
+  var newSpan = document.createElement("SPAN");
+  var checkMark = document.createTextNode("     \u2713");
+  newSpan.className = "close";
+  newSpan.appendChild(checkMark);
+  newli.appendChild(newSpan);
+}
