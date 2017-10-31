d5bd314817e0ea21f10d1e66dc2842bfc38adbe9
rsingh28 <rashmeet.singh28@gmail.com>
2017-10-31 19:55:04 -0400

    Removing the default item from the to-do List and adding the removeElement() method

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Adding a check mark in front of the default element in the list (Repeated in the addAction() function - to add a check mark everytime an element is added)
+// Deleting the element on clicking the check-mark appended to the end of the data entry
 
-var elementList = document.getElementsByTagName("LI");       // Make an array of elements currently in the to-do list 
-for(var i=0 ; i<elementList.length ; i++){
-  var newSpan = document.createElement("SPAN");              // New span element
-  var checkMark = document.createTextNode("     \u2713");    // Creating a check mark using uni-code
-  newSpan.className = "close";                               // Will be used furthur to basically remove the item on clicking the check mark
-  newSpan.appendChild(checkMark);                            // Append the check mark to the new spam element
-  elementList[i].appendChild(newSpan);                       // Append the spam element (the check mark) to the list element 
+function removeElement(remove) {
+	for(var i=0 ; i<remove.length ; i++){
+		remove[i].onclick = function(){
+			var item = this.parentElement;
+			item.style.display="none";
+		}
+	}
 }
 
 // Adding a new element to the list on clicking the button 
@@ -19,13 +19,18 @@ function addAction () {
   document.getElementById("list").appendChild(newli);         // Appending the list item to the main list 
   document.getElementById("action").value = "";               // Setting the text bar to blank
 
-// Adding a check mark in front of the added element 
+  // Adding a check mark in front of the added element 
   
-  var newSpan = document.createElement("SPAN");
-  var checkMark = document.createTextNode("     \u2713");
-  newSpan.className = "close";
-  newSpan.appendChild(checkMark);
-  newli.appendChild(newSpan);
+  var newSpan = document.createElement("SPAN");               // New span element
+  var checkMark = document.createTextNode("     \u2713");     // Creating a check mark using uni-code
+  newSpan.className = "close";                                // Will be used furthur to basically remove the item on clicking the check mark
+  newSpan.appendChild(checkMark);                             // Append the check mark to the new spam element
+  newli.appendChild(newSpan);                                 // Append the spam element (the check mark) to the list element 
+
+  // Calling the removeElement function to remove element if the check mark is clicked 
+
+  var remove = document.getElementsByClassName("close");
+  removeElement(remove);
 }
 
 // Appending to the list on typing in the text box and pressing enter
@@ -36,3 +41,4 @@ pressEnter.addEventListener("keydown", function (e) {
         addAction();
     }
 });
+
