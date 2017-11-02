9bca7d11cd4ef0be2ee46c4a3ff2ae406c3e5d95
rsingh28 <rashmeet.singh28@gmail.com>
2017-11-01 22:36:47 -0400

    Functionality added to dynamically update the number of tasks in the list and change the number dynamically (in a display message)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var count = 0;                                                // This variable holds the number of entries in the list
+
 // Deleting the element on clicking the check-mark appended to the end of the data entry
 
 function removeElement(remove) {
@@ -5,6 +7,20 @@ function removeElement(remove) {
 		remove[i].onclick = function(){                       // Create a fuction to be called on clicking the check mark
 			var item = this.parentElement;                    // Get the parent element of the check mark
 			item.style.display="none";                        // Remove it from the list 
+
+			// The following steps are for updating the message that shows the number of elements in the list
+
+			count--;                                                                // Since the element got removed, decrease 1 from the total number of elements
+			var message = document.querySelector("#count").textContent;             // Get the element with the ID = count and store its value in "message" variable
+			if(count==0){                                                           // Self-expanatory if-else block 
+				message = "You have nothing in you to-do List!!";
+				document.querySelector("#count").textContent=message;               // Display the message 
+			}
+			else{
+				message = "You have " + count + " elements in your to-do List";
+				document.querySelector("#count").textContent=message;
+			}
+			
 		}
 	}
 }
@@ -12,25 +28,38 @@ function removeElement(remove) {
 // Adding a new element to the list on clicking the button 
 
 function addAction () {
-  var action = document.getElementById("action").value;       // Get the text from the text bar
-  var newli = document.createElement('li');                   // Creating a new list item
-  var t = document.createTextNode(action);                    // Create a text node with the text from text bar
-  newli.appendChild(t);                                       // Setting the new list item with the value of text node 
-  document.getElementById("list").appendChild(newli);         // Appending the list item to the main list 
-  document.getElementById("action").value = "";               // Setting the text bar to blank
+	var action = document.getElementById("action").value;       // Get the text from the text bar
+ 	var newli = document.createElement('li');                   // Creating a new list item
+  	var t = document.createTextNode(action);                    // Create a text node with the text from text bar
+  	newli.appendChild(t);                                       // Setting the new list item with the value of text node 
+  	document.getElementById("list").appendChild(newli);         // Appending the list item to the main list 
+  	document.getElementById("action").value = "";               // Setting the text bar to blank
 
-  // Adding a check mark in front of the added element 
+  	// Adding a check mark in front of the added element 
   
-  var newSpan = document.createElement("SPAN");               // New span element
-  var checkMark = document.createTextNode("     \u2713");     // Creating a check mark using uni-code
-  newSpan.className = "close";                                // Will be used furthur to basically remove the item on clicking the check mark
-  newSpan.appendChild(checkMark);                             // Append the check mark to the new spam element
-  newli.appendChild(newSpan);                                 // Append the spam element (the check mark) to the list element 
+  	var newSpan = document.createElement("SPAN");               // New span element
+  	var checkMark = document.createTextNode("     \u2713");     // Creating a check mark using uni-code
+  	newSpan.className = "close";                                // Will be used furthur to basically remove the item on clicking the check mark
+  	newSpan.appendChild(checkMark);                             // Append the check mark to the new spam element
+  	newli.appendChild(newSpan);                                 // Append the spam element (the check mark) to the list element 
 
-  // Calling the removeElement function to remove element if the check mark is clicked 
+  	// The following steps are for updating the message that shows the number of elements in the list
 
-  var remove = document.getElementsByClassName("close");
-  removeElement(remove);
+	count++;                                                                // Since the element got removed, decrease 1 from the total number of elements
+	var message = document.querySelector("#count").textContent;             // Get the element with the ID = count and store its value in "message" variable
+	if(count==0){                                                           // Self-expanatory if-else block 
+		message = "You have nothing in you to-do List!!";
+		document.querySelector("#count").textContent=message;               // Display the message 
+	}
+	else{
+		message = "You have " + count + " elements in your to-do List";
+		document.querySelector("#count").textContent=message;
+	}
+
+  	// Calling the removeElement function to remove element if the check mark is clicked 
+
+  	var remove = document.getElementsByClassName("close");
+  	removeElement(remove);
 }
 
 // Appending to the list on typing in the text box and pressing enter
